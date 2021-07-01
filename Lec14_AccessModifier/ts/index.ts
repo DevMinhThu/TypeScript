20be621dbbf173c15794c07255ca4cf1d8855703
Minh Thư <devminhthu2023@gmail.com>
2021-07-01 11:42:29 +0700

    commit

---
+++ Lec14_AccessModifier/ts/index.ts
@@ -0,0 +1,47 @@
+class KhoaHoc {
+    id: number;
+    ten: string;
+    doDai: number;
+
+    constructor(id: number, ten: string, doDai: number) {
+        this.id = id;
+        this.ten = ten;
+        this.doDai = doDai;
+    }
+
+    xemKhoaHoc() {
+        console.log(this.ten);
+        console.log(`Id: ${this.id}, Ten: ${this.ten}, Do Dai: ${this.doDai}`);
+    }
+}
+
+class KhoaHocLapTrinh extends KhoaHoc {
+    fileDinhkem: string;
+
+    constructor(id: number, ten: string, doDai: number, fileDinhkem: string) {
+        super(id, ten, doDai);
+        this.fileDinhkem = fileDinhkem;
+    }
+
+    xemKhoaHoc() {
+        super.xemKhoaHoc();
+        console.log('File dinh kem: ' + this.fileDinhkem);
+    }
+
+    test() {
+        // truy nhập thuộc tính tên từ bên trong class con
+        console.log(this.ten);
+    }
+}
+
+// === public ===
+
+// test trong class
+let khoa01 = new KhoaHoc(8, "react", 10)
+console.log(khoa01.ten); // truy nhập từ bên ngoài class
+console.log(khoa01.xemKhoaHoc()); // truy nhập từ bên trong class, log ra tên
+
+// class con
+let khoa02 = new KhoaHocLapTrinh(9, 'JS', 13, 'a.zip')
+khoa02.test()
+
